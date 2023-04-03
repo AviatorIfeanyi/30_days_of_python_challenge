05678db472aee7d0391312454bc123b62861123a
AviatorIfeanyi <inboxlegacy@gmail.com>
2023-04-03 22:26:35 +0100

    day_1: refactor code; remove letters from list

--- day_1_2_3.py
+++ day_1_2_3.py
@@ -12,10 +12,20 @@
 # #If all the letters in the list are used in the string, the function should return an empty list (see example #3).
 
 # Code source https://edabit.com/challenge/gH3QMvF3czMDjENkk
-def remove_letters(letters, word):
-		for letter in word:
-			if letter in letters:
-				letters.remove(letter)
-		return letters
+# def remove_letters(letters, word):
+# 		for letter in word:
+# 			if letter in letters:
+# 				letters.remove(letter)
+# 		return letters
 
-print(remove_letters(["b", "b", "l", "l", "g", "n", "o", "a", "w"], "balloon") )
+# print(remove_letters(["b", "b", "l", "l", "g", "n", "o", "a", "w"], "balloon") )
+
+def remove_letters(list_letters, str_word):
+    [list_letters.remove(letter) for letter in str_word if letter in list_letters]
+    
+    return list_letters
+
+letters = ["s", "t", "r", "i", "n", "g", "w"]
+word = 'string'
+
+print(remove_letters(letters, word))
