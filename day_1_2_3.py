f1f6dc0a9103a5d70e177df8f550dd895cb49c21
AviatorIfeanyi <inboxlegacy@gmail.com>
2023-04-03 22:32:37 +0100

    30_days_py_challenge: day_1 complete

--- day_1_2_3.py
+++ day_1_2_3.py
@@ -1,3 +1,5 @@
+## Day 1
+
 # Create a function that takes a list and string. 
 # The function should remove the letters in the string from the list, and return the list.
 
@@ -12,6 +14,7 @@
 # #If all the letters in the list are used in the string, the function should return an empty list (see example #3).
 
 # Code source https://edabit.com/challenge/gH3QMvF3czMDjENkk
+
 # def remove_letters(letters, word):
 # 		for letter in word:
 # 			if letter in letters:
@@ -20,12 +23,24 @@
 
 # print(remove_letters(["b", "b", "l", "l", "g", "n", "o", "a", "w"], "balloon") )
 
-def remove_letters(list_letters, str_word):
-    [list_letters.remove(letter) for letter in str_word if letter in list_letters]
+
+# ===============================
+# Alternative solution
+
+# def remove_letters(list_letters, str_word):
+#     [list_letters.remove(letter) for letter in str_word if letter in list_letters]
     
-    return list_letters
+#     return list_letters
 
-letters = ["s", "t", "r", "i", "n", "g", "w"]
-word = 'string'
+# letters = ["s", "t", "r", "i", "n", "g", "w"]
+# word = 'string'
 
-print(remove_letters(letters, word))
+# print(remove_letters(letters, word))
+
+# ============================================================================================================
+
+
+
+# ==========================================
+# Day 2
+# ==========================================
