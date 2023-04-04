aafd689ccf64d7d2415be3b557dabb77c0ab1fa8
AviatorIfeanyi <inboxlegacy@gmail.com>
2023-04-04 11:48:37 +0100

    day_2 python challenge completed

--- day_1_2_3.py
+++ day_1_2_3.py
@@ -44,3 +44,28 @@
 # ==========================================
 # Day 2
 # ==========================================
+
+# https://edabit.com/challenge/mHLAmj4vmRuXrT8Nb
+
+# Write a function that returns True if two arrays, when combined, form a consecutive sequence. A consecutive sequence is a sequence without any gaps in the integers, e.g. 1, 2, 3, 4, 5 is a consecutive sequence, but 1, 2, 4, 5 is not.
+# Examples
+
+# consecutive_combo([7, 4, 5, 1], [2, 3, 6]) ➞ True
+
+# consecutive_combo([1, 4, 6, 5], [2, 7, 8, 9]) ➞ False
+
+# consecutive_combo([1, 4, 5, 6], [2, 3, 7, 8, 10]) ➞ False
+
+# consecutive_combo([44, 46], [45]) ➞ True
+
+# Notes
+
+#     The input lists will have unique values.
+#     The input lists can be in any order.
+
+# def consecutive_combo(lst1, lst2):
+#   lst3 = set(sorted(lst1 + lst2) )
+  
+#   return list(range(min(lst1), max(lst2)+1)) == list(lst3)
+
+# print(consecutive_combo([3, 1, 4, 6, 5], [2, 7, 8, 9]))
