bf3a95b97c101b94fa7009da4414042eb4425c09
AviatorIfeanyi <inboxlegacy@gmail.com>
2023-04-15 00:54:08 +0100

    python challenge day 4,5,6

--- day_4_5_6.py
+++ day_4_5_6.py
@@ -18,3 +18,36 @@
 # print(censor_string("Today is a Wednesday!", ["Today", "a"], "-") )
 
 # # censor_string("The cow jumped over the moon.", ["cow", "over"], "*"), "The *** jumped **** the moon.")
+
+
+# ===============================================
+# day_5
+# Write a function to check whether a passed string is a palindrome or not 
+# ===============================================
+
+# def check_palindrome(palindrome):
+#   reverse_palindrome = ''
+#   palindrome_length = len(palindrome)
+  
+#   for _ in palindrome:
+#     reverse_palindrome += palindrome[palindrome_length - 1]
+#     palindrome_length -= 1
+    
+#   return palindrome == reverse_palindrome
+
+# print(check_palindrome('redder'))
+
+# ================================================
+# day_6
+# ================================================
+
+# def is_disarium(arg):
+#   lst_number = list(str(arg))
+#   number_counter = 0
+#   for int_number in lst_number:
+#     number_counter+=int(int_number) ** (lst_number.index(int_number) + 1) 
+#   return number_counter == arg
+
+# print( is_disarium(75) )
+
+
